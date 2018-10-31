8eeb9fc78ac2c7b29f691e9773a0e42b9288af83
William O'Beirne <wbobeirne@gmail.com>
2018-10-31 17:05:35 -0400

    New color palette & redesigned splash screen (#170)
    * Use name and icon logos where possible. Add favicon and other general web meta info.
    
    * Unused import
    
    * Update icons and logos.
    
    * Variablize all colors. Switch to purple primary.
    
    * Redesign homepage. Slight color tweaks.
    
    * Fix blue button

--- frontend/client/components/Home/index.tsx
+++ frontend/client/components/Home/index.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Icon } from 'antd';
 import HeaderDetails from 'components/HeaderDetails';
-import TeamsSvg from 'static/images/intro-teams.svg';
-import FundingSvg from 'static/images/intro-funding.svg';
-import CommunitySvg from 'static/images/intro-community.svg';
+import Rocket from 'static/images/rocket.svg';
 import './style.less';
-
-const introBlobs = [
-  {
-    Svg: TeamsSvg,
-    text: 'Developers and teams propose projects for improving the ecosystem',
-  },
-  {
-    Svg: FundingSvg,
-    text: 'Projects are funded by the community and paid as it’s built',
-  },
-  {
-    Svg: CommunitySvg,
-    text: 'Open discussion and project updates bring devs and the community together',
-  },
-];
 
 export default class Home extends React.Component {
   render() {
@@ -32,38 +14,27 @@ export default class Home extends React.Component {
           improvements"
         />
         <div className="Home-hero">
-          <h1 className="Home-hero-title">
-            Decentralized funding for <br /> Blockchain ecosystem improvements
-          </h1>
-
-          <div className="Home-hero-buttons">
-            <Link className="Home-hero-buttons-button is-primary" to="/create">
-              Propose a Project
-            </Link>
-            <Link className="Home-hero-buttons-button" to="/proposals">
-              Explore Projects
-            </Link>
+          <div className="Home-hero-background">
+            <div className="Home-hero-background-planets">
+              <div className="Home-hero-background-planets-planet is-small" />
+              <div className="Home-hero-background-planets-planet is-large" />
+            </div>
+            <Rocket className="Home-hero-background-rocket" />
           </div>
 
-          <button className="Home-hero-scroll">
-            Learn More
-            <Icon type="down" />
-          </button>
-        </div>
+          <div className="Home-hero-inner">
+            <h1 className="Home-hero-title">
+              Decentralized funding for <br /> Blockchain ecosystem improvements
+            </h1>
 
-        <div className="Home-intro">
-          <h3 className="Home-intro-text">
-            Grant.io organizes creators and community members to incentivize ecosystem
-            improvements
-          </h3>
-
-          <div className="Home-intro-blobs">
-            {introBlobs.map((blob, i) => (
-              <div className="Home-intro-blobs-blob" key={i}>
-                <blob.Svg />
-                <p>{blob.text}</p>
-              </div>
-            ))}
+            <div className="Home-hero-buttons">
+              <Link className="Home-hero-buttons-button is-primary" to="/create">
+                Propose a Project
+              </Link>
+              <Link className="Home-hero-buttons-button" to="/proposals">
+                Explore Projects
+              </Link>
+            </div>
           </div>
         </div>
       </div>
