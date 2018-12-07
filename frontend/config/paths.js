9fcbe7b0e9ef272bcf04a9e9855cad880b8ced3f
Aaron <aaron.m.stricker@gmail.com>
2018-12-07 09:17:09 -0600

    FE remove eth deps + contracts

--- frontend/config/paths.js
+++ frontend/config/paths.js
@@ -3,14 +3,11 @@ const fs = require('fs');
 const findRoot = require('find-root');
 
 const appDirectory = fs.realpathSync(process.cwd());
-// truffle exec cwd moves to called js, so make sure we are on root
 const appRoot = findRoot(appDirectory);
 const resolveApp = relativePath => path.resolve(appRoot, relativePath);
 
 const paths = {
   clientBuild: resolveApp('build/client'),
-  contractsBase: resolveApp('../contract'),
-  contractsBuild: resolveApp('../contract/build/contracts'),
   dotenv: resolveApp('.env'),
   logs: resolveApp('logs'),
   publicPath: '/static/',
