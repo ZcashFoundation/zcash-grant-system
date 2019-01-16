48912c95ccfbf2ae2cab01e1828ce4a2fe89ccab
AMStrix <aaron.m.stricker@gmail.com>
2019-01-16 17:19:34 -0600

    markdown driven static pages (#40)

--- frontend/client/pages/tos.tsx
+++ frontend/client/pages/tos.tsx
@@ -1,2 +1,7 @@
-import Tos from 'components/Tos';
+import React from 'react';
+import MarkdownPage from 'components/MarkdownPage';
+import TOS from 'static/markdown/TOS.md';
+
+const Tos = () => <MarkdownPage markdown={TOS} />;
+
 export default Tos;
