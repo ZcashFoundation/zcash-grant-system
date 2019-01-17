8dff777e956560960ce2bffdaa115d1fa6014c39
Aaron <aaron.m.stricker@gmail.com>
2019-01-17 14:07:49 -0600

    blockchain: default & required env vars

--- blockchain/src/env.ts
+++ blockchain/src/env.ts
@@ -2,22 +2,38 @@ import dotenv from "dotenv";
 dotenv.load();
 
 // Maps to .env.example variables, plus any node ones we use
-interface CustomEnvironment {
-  API_SECRET_HASH: string;
-  API_SECRET_KEY: string;
+// fill in sensible defaults, falsy values will throw if not set
+const DEFAULTS = {
+  API_SECRET_HASH: "",
+  API_SECRET_KEY: "",
 
-  WEBHOOK_URL: string;
-  PORT: string;
+  WEBHOOK_URL: "",
+  PORT: "5051",
 
-  ZCASH_NODE_URL: string;
-  ZCASH_NODE_USERNAME: string;
-  ZCASH_NODE_PASSWORD: string;
-  MINIMUM_BLOCK_CONFIRMATIONS: string;
+  ZCASH_NODE_URL: "",
+  ZCASH_NODE_USERNAME: "",
+  ZCASH_NODE_PASSWORD: "",
+  MINIMUM_BLOCK_CONFIRMATIONS: "6",
 
-  SPROUT_ADDRESS: string;
-  SPROUT_VIEWKEY: string;
-  BIP32_XPUB: string;
-  BIP44_ACCOUNT: string;
-}
+  SPROUT_ADDRESS: "",
+  SPROUT_VIEWKEY: "",
+  BIP32_XPUB: ""
+  // can't find any refs to this
+  // BIP44_ACCOUNT: ""
+};
 
-export default process.env as any as CustomEnvironment;
+type CustomEnvironment = typeof DEFAULTS;
+
+Object.entries(DEFAULTS).forEach(([k, v]) => {
+  if (!process.env[k]) {
+    const defVal = (DEFAULTS as any)[k];
+    if (defVal) {
+      console.log(`Using default environment variable ${k}="${defVal}"`);
+      process.env[k] = defVal;
+    } else {
+      throw new Error(`Missing required environment variable ${k}`);
+    }
+  }
+});
+
+export default (process.env as any) as CustomEnvironment;
