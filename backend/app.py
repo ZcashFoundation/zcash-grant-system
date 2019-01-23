5efad81170e6a3f117a66955f8e8f63f6733414c
Will O'Beirne <wbobeirne@gmail.com>
2019-01-23 09:59:42 -0500

    pep8 formatting

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,10 @@ from grant.blockchain.bootstrap import send_bootstrap_data
 
 app = create_app()
 
+
 @app.before_first_request
 def bootstrap_watcher():
-  try:
-    send_bootstrap_data()
-  except:
-    print('Failed to send bootstrap data, watcher must be offline')
+    try:
+        send_bootstrap_data()
+    except:
+        print('Failed to send bootstrap data, watcher must be offline')
