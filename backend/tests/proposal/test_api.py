1e8b63d2a1b6301e2221572477a81d934e61d676
Daniel Ternyak <dternyak@gmail.com>
2019-01-28 19:06:24 -0600

    fix test

--- backend/tests/proposal/test_api.py
+++ backend/tests/proposal/test_api.py
@@ -114,7 +114,7 @@ class TestProposalAPI(BaseProposalCreatorConfig):
         self.mark_user_not_verified()
         self.proposal.status = "DRAFT"
         resp = self.app.put("/api/v1/proposals/{}/publish".format(self.proposal.id))
-        self.assert400(resp)
+        self.assert403(resp)
 
     # /
     def test_get_proposals(self):
