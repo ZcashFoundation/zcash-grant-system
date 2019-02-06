de1f1080a5b81e3e3318f040efde44533447d789
Will O'Beirne <wbobeirne@gmail.com>
2019-02-06 10:54:43 -0500

    Setup components and routes for contributions.

---
+++ admin/src/components/ContributionDetail/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ContributionDetail extends React.Component<{}> {
+  render() {
+    return <h1>Contribution Detail</h1>;
+  }
+}
+
+export default ContributionDetail;
