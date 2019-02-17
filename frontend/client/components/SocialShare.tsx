5e2fdbfea4de3cdcc6c96ef6cb49191a2e5ea5b9
Daniel Ternyak <dternyak@gmail.com>
2019-02-17 15:57:57 -0600

    Use Modal to present social share option instead of direct pop-up

--- frontend/client/components/SocialShare.tsx
+++ frontend/client/components/SocialShare.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import './SocialShare.less';
+import { Modal } from 'antd';
+import CopyInput from 'components/CopyInput';
 
 interface TypeOptions {
   className: string;
+  humanName: string;
   url: (url: string, title: string, text: string) => string;
 }
 
 const types: { [index: string]: TypeOptions } = {
   twitter: {
-    className: 'fab fa-twitter-square',
+    humanName: 'Twitter',
+    className: 'fab fa-twitter',
     url: (url: string, _: string, text: string) =>
       `https://twitter.com/intent/tweet?url=${url}&text=${text}`,
   },
   reddit: {
-    className: 'fab fa-reddit-square',
+    humanName: 'Reddit',
+    className: 'fab fa-reddit',
     url: (url: string, title: string) =>
       `https://reddit.com/submit?url=${url}&title=${title}`,
   },
   facebook: {
-    className: 'fab fa-facebook-square',
+    humanName: 'Facebook',
+    className: 'fab fa-facebook',
     url: (url: string) => `http://www.facebook.com/sharer.php?u=${url}`,
   },
   linkedin: {
-    className: 'fab fa-linkedin-square',
+    humanName: 'LinkedIn',
+    className: 'fab fa-linkedin',
     url: (url: string, title: string, text: string) =>
       `https://www.linkedin.com/shareArticle?mini=true&url=${url}&title=${title}&summary=${text}`,
   },
@@ -34,64 +41,62 @@ interface OwnProps {
   title: string;
 }
 
+interface State {
+  openModal: boolean;
+  socialKey: string;
+}
+
 type Props = OwnProps;
 
-export default class SocialShare extends React.Component<Props> {
+export default class SocialShare extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      openModal: false,
+      socialKey: 'twitter',
+    };
+  }
+
   render() {
     let { url, title, text } = this.props;
+    const { openModal, socialKey } = this.state;
     url = url.replace(`localhost:${process.env.PORT}`, 'grants.zfnd.org');
     url = encodeURIComponent(url);
     title = encodeURIComponent(title);
     text = encodeURIComponent(text);
     return (
       <div className="SocialShare">
+        <Modal
+          title={
+            <a className={`SocialShare-icon is-${socialKey}`}>
+              <i className={types[socialKey].className} />
+              {' ' + types[socialKey].humanName}
+            </a>
+          }
+          visible={openModal}
+          footer={null}
+          onCancel={() => this.setState({ openModal: false })}
+        >
+          <CopyInput label={''} value={types[socialKey].url(url, title, text)} />
+        </Modal>
         {Object.keys(types).map(key => {
           const opts = types[key];
           return (
             <a
-              target="popup"
-              onClick={() => windowOpen(opts.url(url, title, text))}
+              onClick={() =>
+                this.setState({
+                  openModal: true,
+                  socialKey: key,
+                })
+              }
               key={key}
               className={`SocialShare-button is-${key}`}
             >
-              <i className={opts.className} />
+              <i className={opts.className + '-square'} />
             </a>
           );
         })}
       </div>
     );
   }
-}
-
-function windowOpen(url: string, name = 'Share', width = 550, height = 500) {
-  const left =
-    window.outerWidth / 2 + (window.screenX || window.screenLeft || 0) - width / 2;
-  const top =
-    window.outerHeight / 2 + (window.screenY || window.screenTop || 0) - height / 2;
-
-  const config: { [index: string]: any } = {
-    height,
-    width,
-    left,
-    top,
-    location: 'no',
-    toolbar: 'no',
-    status: 'no',
-    directories: 'no',
-    menubar: 'no',
-    scrollbars: 'yes',
-    resizable: 'no',
-    centerscreen: 'yes',
-    chrome: 'yes',
-  };
-
-  const shareDialog = window.open(
-    url,
-    name,
-    Object.keys(config)
-      .map(key => `${key}=${config[key]}`)
-      .join(', '),
-  );
-
-  return shareDialog;
 }
