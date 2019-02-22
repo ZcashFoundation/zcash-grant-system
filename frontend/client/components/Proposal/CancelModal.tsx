38cac2b334e238c7a53cad00edf6e423f27887e7
Will O'Beirne <wbobeirne@gmail.com>
2019-02-22 13:59:33 -0500

    Remove edit action, make cancel an explanation to contact support.

--- frontend/client/components/Proposal/CancelModal.tsx
+++ frontend/client/components/Proposal/CancelModal.tsx
@@ -1,85 +1,42 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Modal, Alert } from 'antd';
+import { Modal } from 'antd';
+import { Link } from 'react-router-dom';
 import { Proposal } from 'types';
-import { AppState } from 'store/reducers';
 
-interface OwnProps {
+interface Props {
   proposal: Proposal;
   isVisible: boolean;
   handleClose(): void;
 }
 
-interface StateProps {
-  isRefundActionPending: boolean;
-  refundActionError: string;
-}
-
-type Props = StateProps & OwnProps;
-
-class CancelModal extends React.Component<Props> {
-  componentDidUpdate() {
-    // TODO: Close on success of action
-  }
-
+export default class CancelModal extends React.Component<Props> {
   render() {
-    const { isVisible, isRefundActionPending, refundActionError } = this.props;
-    const hasContributors = false; // TODO: Determine if it has contributors from proposal
-    const disabled = isRefundActionPending;
+    const { isVisible, handleClose } = this.props;
 
     return (
       <Modal
         title={<>Cancel proposal</>}
         visible={isVisible}
-        okText="Confirm"
-        cancelText="Never mind"
-        onOk={this.cancelProposal}
-        onCancel={this.closeModal}
-        okButtonProps={{ type: 'danger', loading: disabled }}
-        cancelButtonProps={{ disabled }}
+        okText="OK"
+        cancelText="Cancel"
+        onOk={handleClose}
+        onCancel={handleClose}
       >
         <p>
-          Are you sure you would like to cancel this proposal?{' '}
-          <strong>This cannot be undone</strong>.
+          Are you sure you would like to cancel this proposal, and refund any
+          contributors? <strong>This cannot be undone</strong>.
         </p>
         <p>
           Canceled proposals cannot be deleted and will still be viewable by contributors
           or anyone with a direct link. However, they will be de-listed everywhere else on
           ZF Grants.
         </p>
-        {hasContributors && (
-          <p>
-            Should you choose to cancel, we highly recommend posting an update to let your
-            contributors know why you’ve decided to do so.
-          </p>
-        )}
-        {refundActionError && (
-          <Alert
-            type="error"
-            message="Failed to cancel proposal"
-            description={refundActionError}
-            showIcon
-          />
-        )}
+        <p>
+          If you're sure you'd like to cancel, please{' '}
+          <Link to="/contact">contact support</Link> to let us know. Canceling can only be
+          done by site admins.
+        </p>
       </Modal>
     );
   }
-
-  private closeModal = () => {
-    if (!this.props.isRefundActionPending) {
-      this.props.handleClose();
-    }
-  };
-
-  private cancelProposal = () => {
-    console.warn('TODO - implement cancelProposal');
-  };
 }
-
-export default connect<StateProps, {}, OwnProps, AppState>(state => {
-  console.warn('TODO - redux isRefundActionPending/refundActionError?', state);
-  return {
-    isRefundActionPending: false,
-    refundActionError: '',
-  };
-})(CancelModal);
