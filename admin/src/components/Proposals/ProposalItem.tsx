625a6cab990f805aceb6fe42771a8c2ed7260498
Daniel Ternyak <dternyak@gmail.com>
2019-02-27 16:50:53 -0600

    don't show popovers, remove delete, add tsc to admin pre-commit

--- admin/src/components/Proposals/ProposalItem.tsx
+++ admin/src/components/Proposals/ProposalItem.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { view } from 'react-easy-state';
-import { Popconfirm, Tag, Tooltip, List } from 'antd';
+import { Tag, Tooltip, List } from 'antd';
 import { Link } from 'react-router-dom';
-import store from 'src/store';
 import { Proposal, PROPOSAL_STATUS } from 'src/types';
 import { PROPOSAL_STATUSES, PROPOSAL_STAGES, getStatusById } from 'util/statuses';
 import { formatDateSeconds } from 'util/time';
@@ -16,45 +15,32 @@ class ProposalItemNaked extends React.Component<Proposal> {
     const p = this.props;
     const status = getStatusById(PROPOSAL_STATUSES, p.status);
     const stage = getStatusById(PROPOSAL_STAGES, p.stage);
-    const actions = [
-      <Popconfirm
-        key="delete"
-        onConfirm={this.handleDelete}
-        title="Are you sure?"
-        okText="Delete"
-        okType="danger"
-        placement="left"
-      >
-        <a>delete</a>
-      </Popconfirm>,
-    ];
 
     return (
-      <List.Item key={p.proposalId} className="ProposalItem" actions={actions}>
+      <List.Item key={p.proposalId} className="ProposalItem">
         <Link to={`/proposals/${p.proposalId}`}>
           <h2>
             {p.title || '(no title)'}
             <Tooltip title={status.hint}>
               <Tag color={status.tagColor}>{status.tagDisplay}</Tag>
             </Tooltip>
-            {p.status === PROPOSAL_STATUS.LIVE &&
+            {p.status === PROPOSAL_STATUS.LIVE && (
               <Tooltip title={stage.hint}>
                 <Tag color={stage.tagColor}>{stage.tagDisplay}</Tag>
               </Tooltip>
-            }
+            )}
           </h2>
           <p>Created: {formatDateSeconds(p.dateCreated)}</p>
           <p>{p.brief}</p>
           {p.rfp && (
-            <p>Submitted for RFP: <strong>{p.rfp.title}</strong></p>
+            <p>
+              Submitted for RFP: <strong>{p.rfp.title}</strong>
+            </p>
           )}
         </Link>
       </List.Item>
     );
   }
-  private handleDelete = () => {
-    store.deleteProposal(this.props.proposalId);
-  };
 }
 
 const ProposalItem = view(ProposalItemNaked);
