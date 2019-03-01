5b224c74891b034c00cfd7369abc9d6b9190e83a
Daniel Ternyak <dternyak@gmail.com>
2019-02-28 18:11:52 -0600

    create command to set admins from CLI

--- backend/grant/user/commands.py
+++ backend/grant/user/commands.py
@@ -4,25 +4,24 @@ from flask.cli import with_appcontext
 from .models import User, db
 
 
-@click.command()
-@click.argument('identity')
-@with_appcontext
-def delete_user(identity):
-    print(identity)
-    user = None
-    if str.isdigit(identity):
-        user = User.get_by_id(identity)
-    else:
-        user = User.get_by_email(identity)
-
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-        click.echo(f'Succesfully deleted {user.display_name} (uid {user.id})')
-    else:
-        raise click.BadParameter('Invalid user identity. Must be a userid, ' \
-                                 'account address, or email address of an ' \
-                                 'existing user.')
+# @click.command()
+# @click.argument('identity')
+# @with_appcontext
+# def delete_user(identity):
+#     print(identity)
+#     if str.isdigit(identity):
+#         user = User.get_by_id(identity)
+#     else:
+#         user = User.get_by_email(identity)
+#
+#     if user:
+#         db.session.delete(user)
+#         db.session.commit()
+#         click.echo(f'Succesfully deleted {user.display_name} (uid {user.id})')
+#     else:
+#         raise click.BadParameter('Invalid user identity. Must be a userid, ' \
+#                                  'account address, or email address of an ' \
+#                                  'existing user.')
 
 
 @click.command()
@@ -30,7 +29,6 @@ def delete_user(identity):
 @with_appcontext
 def set_admin(identity):
     print("Setting admin to user with identity: " + identity)
-
     if str.isdigit(identity):
         user = User.get_by_id(identity)
     else:
