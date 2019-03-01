8fe91b9fb2aed4fde5016a143baf5f61cb8fad98
Daniel Ternyak <dternyak@gmail.com>
2019-02-28 18:09:40 -0600

    create command to set admins from CLI

--- backend/grant/user/commands.py
+++ backend/grant/user/commands.py
@@ -23,3 +23,25 @@ def delete_user(identity):
         raise click.BadParameter('Invalid user identity. Must be a userid, ' \
                                  'account address, or email address of an ' \
                                  'existing user.')
+
+
+@click.command()
+@click.argument('identity')
+@with_appcontext
+def set_admin(identity):
+    print("Setting admin to user with identity: " + identity)
+
+    if str.isdigit(identity):
+        user = User.get_by_id(identity)
+    else:
+        user = User.get_by_email(identity)
+
+    if user:
+        user.set_admin(True)
+        db.session.add(user)
+        db.session.commit()
+        click.echo(f'Successfully set {user.display_name} (uid {user.id}) to admin')
+    else:
+        raise click.BadParameter('''Invalid user identity. Must be a userid, 
+                                 'account address, or email address of an  
+                                 'existing user.''')
