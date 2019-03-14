41a2e866adc081a3f188299f580d5eccf537c0a8
Daniel Ternyak <dternyak@gmail.com>
2019-03-14 15:22:03 -0500

    logging in blockchain interact

--- backend/grant/utils/requests.py
+++ backend/grant/utils/requests.py
@@ -1,6 +1,8 @@
 import requests
+from flask import current_app
 
 from grant.settings import BLOCKCHAIN_REST_API_URL, BLOCKCHAIN_API_SECRET, E2E_TESTING
+from grant.utils.exceptions import ValidationException
 
 
 ### REST API ###
@@ -15,28 +17,42 @@ def handle_res(res):
 def blockchain_get(path, params=None):
     if E2E_TESTING:
         return blockchain_rest_e2e(path, params)
-    res = requests.get(
-        f'{BLOCKCHAIN_REST_API_URL}{path}',
-        headers={'authorization': BLOCKCHAIN_API_SECRET},
-        params=params,
-    )
-    return handle_res(res)
+    try:
+        res = requests.get(
+            f'{BLOCKCHAIN_REST_API_URL}{path}',
+            headers={'authorization': BLOCKCHAIN_API_SECRET},
+            params=params,
+        )
+        return handle_res(res)
+    except Exception as e:
+        current_app.logger.error(f"Unable to contact node: {e}")
+        raise e
 
 
 def validate_blockchain_get(path, params=None):
-    res = blockchain_get(path, params)
-    return res.get('valid')
+    try:
+        res = blockchain_get(path, params)
+    except Exception:
+        raise ValidationException('Unable to validate zcash address right now, try again later')
+    if not res.get('valid'):
+        raise ValidationException('Invalid Zcash address')
+
+    return True
 
 
 def blockchain_post(path, data=None):
     if E2E_TESTING:
         return blockchain_rest_e2e(path, data)
-    res = requests.post(
-        f'{BLOCKCHAIN_REST_API_URL}{path}',
-        headers={'authorization': BLOCKCHAIN_API_SECRET},
-        json=data,
-    )
-    return handle_res(res)
+    try:
+        res = requests.post(
+            f'{BLOCKCHAIN_REST_API_URL}{path}',
+            headers={'authorization': BLOCKCHAIN_API_SECRET},
+            json=data,
+        )
+        return handle_res(res)
+    except Exception as e:
+        current_app.logger.error(f"Unable to contact node: {e}")
+        raise e
 
 
 def blockchain_rest_e2e(path, data):
