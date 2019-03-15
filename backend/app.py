993e9477c897fe9c02b780e5921c6ed785528cff
Will O'Beirne <wbobeirne@gmail.com>
2019-03-14 23:04:07 -0400

    Repeatedly attempt to bootstrap watcher until it happens.

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
+from flask import request
 from grant.app import create_app
 from grant.blockchain.bootstrap import send_bootstrap_data
 
@@ -8,6 +9,10 @@ app = create_app()
 
 @app.before_first_request
 def bootstrap_watcher():
+    # Don't call bootstrap if the first request to us was bootstrap
+    if 'blockchain/bootstrap' in request.path:
+        return
+
     try:
         send_bootstrap_data()
     except:
