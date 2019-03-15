5a3d7e34b5a0a376629537f6994bf02088acb639
Will O'Beirne <wbobeirne@gmail.com>
2019-03-14 23:05:35 -0400

    Get rid of first request bootstrap, watcher polls now.

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
-from flask import request
 from grant.app import create_app
-from grant.blockchain.bootstrap import send_bootstrap_data
 
 app = create_app()
-
-
-@app.before_first_request
-def bootstrap_watcher():
-    # Don't call bootstrap if the first request to us was bootstrap
-    if 'blockchain/bootstrap' in request.path:
-        return
-
-    try:
-        send_bootstrap_data()
-    except:
-        print('Failed to send bootstrap data, watcher must be offline')
