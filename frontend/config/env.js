0f49a73ae38140287da4590093f42e60c65652f9
Will O'Beirne <wbobeirne@gmail.com>
2019-03-18 15:02:47 -0400

    Fix tslint

--- frontend/config/env.js
+++ frontend/config/env.js
@@ -57,11 +57,11 @@ module.exports = () => {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 3000,
     PROPOSAL_STAKING_AMOUNT: process.env.PROPOSAL_STAKING_AMOUNT,
+    PROPOSAL_TARGET_MAX: process.env.PROPOSAL_TARGET_MAX || '10000',
     PUBLIC_HOST_URL: process.env.PUBLIC_HOST_URL,
     SENTRY_DSN: process.env.SENTRY_DSN || null,
     SENTRY_RELEASE: process.env.SENTRY_RELEASE || undefined,
     TESTNET: process.env.TESTNET || false,
-    PROPOSAL_TARGET_MAX: process.env.PROPOSAL_TARGET_MAX || '10000',
   };
 
   // Stringify all values so we can feed into Webpack DefinePlugin
