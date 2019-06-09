a322ad7db3ab649532511583944a29ed575b0ef0
Daniel Ternyak <dternyak@gmail.com>
2019-06-09 09:54:26 -0500

    removed unused import

--- frontend/client/modules/proposals/reducers.ts
+++ frontend/client/modules/proposals/reducers.ts
@@ -9,7 +9,7 @@ import {
   LoadableProposalPage,
   Moreable,
 } from 'types';
-import { PROPOSAL_SORT, PROPOSAL_STAGE } from 'api/constants';
+import { PROPOSAL_SORT } from 'api/constants';
 
 export interface ProposalDetail extends Proposal {
   isRequestingPayout: boolean;
