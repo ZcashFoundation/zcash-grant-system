c8e75a3e8f68330582837b72b5a72a53d605a9db
Daniel Ternyak <dternyak@gmail.com>
2019-12-10 23:44:18 -0600

    Hookup Proposal Tutorial

---
+++ frontend/client/pages/proposal-tutorial.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import MarkdownPage from 'components/MarkdownPage';
+import PROPOSAL_TUTORIAL from 'static/markdown/PROPOSAL_TUTORIAL.md';
+
+const ProposalTutorial = () => <MarkdownPage markdown={PROPOSAL_TUTORIAL} />;
+
+export default ProposalTutorial;
