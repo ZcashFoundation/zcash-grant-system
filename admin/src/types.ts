22ffcb5ef7d6d30b213b06bcd14733b2e84b5109
Danny Skubak <skubakdj@gmail.com>
2020-02-26 14:08:24 -0500

    handle archived proposal status

--- admin/src/types.ts
+++ admin/src/types.ts
@@ -74,6 +74,7 @@ export interface ProposalArbiter {
 export enum PROPOSAL_STATUS {
   DRAFT = 'DRAFT',
   LIVE_DRAFT = 'LIVE_DRAFT',
+  ARCHIVED = 'ARCHIVED',
   PENDING = 'PENDING',
   DISCUSSION = 'DISCUSSION',
   APPROVED = 'APPROVED',
