206ed2e63a0cd2cab9074a259f3c0d7e63a86d31
Daniel Ternyak <dternyak@gmail.com>
2021-04-13 23:55:08 -0700

    adjust default proposal content

--- backend/grant/proposal/models.py
+++ backend/grant/proposal/models.py
@@ -338,37 +338,42 @@ class ProposalRevision(db.Model):
 
 
 def default_proposal_content():
-    return """# Applicant background
+    return """### If you have any doubts about the questions below, please reach out to anyone on the ZOMG on the [Zcash forums](https://forum.zcashcommunity.com/). 
 
-Summarize you and/or your team’s background and experience. Demonstrate that you have the skills and expertise necessary for the project that you’re proposing. Institutional bona fides are not required, but we want to hear about your track record.
+# Description of Problem or Opportunity  
+In addition to describing the problem/opportunity, please give a sense of how serious or urgent of a need you believe this to be. What evidence do you have? What validation have you already done, or how do you think you could validate this? 
 
-# Motivation and overview
+# Proposed Solution 
+Describe the solution at a high level. Please be specific about who the users and stakeholders are and how they would interact with your solution. E.g. retail ZEC holders, Zcash core devs, wallet devs, DeFi users, potential Zcash community participants.  
 
-What are your high-level goals? Why are they important? How is your project connected to [ZF’s mission](https://www.zfnd.org/about/#mission) and priorities? Whose needs will it serve?
+# Solution Format 
+What is the exact form of the deliverable you’re creating? E.g. code shipped within the zcashd and zebra code bases, a website, a feature within a wallet, a text/markdown file, user manuals, etc.
 
 # Technical approach
-
 Dive into the _how_ of your project. Describe your approaches, components, workflows, methodology, etc. Bullet points and diagrams are appreciated!
 
+# How big of a problem would it be to not solve this problem? 
+
 # Execution risks
+What obstacles do you expect? What is most likely to go wrong? Which unknown factors or dependencies could jeopardize success? Who would have to incorporate your work in order for it to be usable?
 
-What obstacles do you expect? What is most likely to go wrong? Which unknown factors or dependencies could jeopardize success? What are your contingency plans? Will subsequent activities be required to maximize impact?
 
-# Downsides
-
+# Unintended Consequences Downsides
 What are the negative ramifications if your project is successful? Consider usability, stability, privacy, integrity, availability, decentralization, interoperability, maintainability, technical debt, requisite education, etc.
 
 # Evaluation plan
+What metrics for success can you share with the community once you’re done? In addition to quantitative metrics, what qualitative metrics do you think you could report?
 
-What will your project look like if successful? How will we be able to tell? Include quantifiable metrics if possible.
 
-# Tasks and schedule
-
+# Schedule and Milestones
 What is your timeline for the project? Include concrete milestones and the major tasks required to complete each milestone.
 
-# Budget and justification
+# Budget and Payout Timeline
 
-How much funding do you need, and how will it be allocated (e.g., compensation for your effort, specific equipment, specific external services)? Specify a total cost, break it up into budget items, and explain the rationale for each. Feel free to present multiple options in terms of scope and cost.
+How much funding do you need, and how will it be allocated (e.g., compensation for your effort, specific equipment, specific external services)? Please tie your payout timelines to the milestones presented in the previous step. Convention has been for applicants to base their budget on hours of work and an hourly rate, but we are open to proposals based on the value of outcomes instead.  
+
+# Applicant background
+Summarize you and/or your team’s background and experience. Demonstrate that you have the skills and expertise necessary for the project that you’re proposing. Institutional bona fides are not required, but we want to hear about your track record.
 
 """
 
